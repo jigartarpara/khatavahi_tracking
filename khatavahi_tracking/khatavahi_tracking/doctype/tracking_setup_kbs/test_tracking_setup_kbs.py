3362d4a0e33620c51816493bd30d91aa7bf35898
Hiren Savaliya <dev101@khatavahi.in>
2026-02-19 13:10:49 +0530

    User Log KBS and Tracking Setup KBS doc create

---
+++ khatavahi_tracking/khatavahi_tracking/doctype/tracking_setup_kbs/test_tracking_setup_kbs.py
@@ -0,0 +1,9 @@
+# Copyright (c) 2026, Khatavahi Tracking and Contributors
+# See license.txt
+
+# import frappe
+from frappe.tests.utils import FrappeTestCase
+
+
+class TestTrackingSetupKBS(FrappeTestCase):
+	pass
---
+++ khatavahi_tracking/khatavahi_tracking/doctype/tracking_setup_kbs/tracking_setup_kbs.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2026, Khatavahi Tracking and contributors
+// For license information, please see license.txt
+
+// frappe.ui.form.on("Tracking Setup KBS", {
+// 	refresh(frm) {
+
+// 	},
+// });
