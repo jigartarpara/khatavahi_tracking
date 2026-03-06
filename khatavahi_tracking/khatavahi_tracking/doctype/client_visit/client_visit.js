5b4f4828acb720a7ea1b1edb853359a0a05feabb
Hiren Savaliya <dev101@khatavahi.in>
2026-03-06 14:02:14 +0530

    feat: Implement dynamic party selection for Client Visit and add geo-location fields to Opportunity.

--- khatavahi_tracking/khatavahi_tracking/doctype/client_visit/client_visit.js
+++ khatavahi_tracking/khatavahi_tracking/doctype/client_visit/client_visit.js
@@ -1,8 +1,22 @@
 // Copyright (c) 2026, Khatavahi Tracking and contributors
 // For license information, please see license.txt
 
-// frappe.ui.form.on("Client Visit", {
-// 	refresh(frm) {
+frappe.ui.form.on("Client Visit", {
+	refresh(frm) {
+		frm.trigger("set_dynamic_field_label");
+	},
+	visit_for(frm) {
+		frm.trigger("set_dynamic_field_label");
+	},
+	set_dynamic_field_label(frm) {
+		frm.set_df_property("party_name", "label", "Party Name");
 
-// 	},
-// });
+		if (frm.doc.visit_for == "Opportunity") {
+			frm.set_df_property("party_name", "label", "Opportunity");
+			frm.fields_dict.party_name.get_query = null;
+		} else if (frm.doc.visit_for == "Customer") {
+			frm.set_df_property("party_name", "label", "Customer");
+			frm.fields_dict.party_name.get_query = null;
+		}
+	}
+});
