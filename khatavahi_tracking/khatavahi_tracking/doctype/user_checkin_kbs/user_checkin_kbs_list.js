694236944b6d326f682d00ae4a6f2104f4890dbf
Hiren Savaliya <dev101@khatavahi.in>
2026-04-14 18:20:19 +0530

    feat: disable creation and remove primary action buttons for tracking doctype list views

--- khatavahi_tracking/khatavahi_tracking/doctype/user_checkin_kbs/user_checkin_kbs_list.js
+++ khatavahi_tracking/khatavahi_tracking/doctype/user_checkin_kbs/user_checkin_kbs_list.js
@@ -1,5 +1,10 @@
 frappe.listview_settings["User Checkin KBS"] = {
+	onload: function (listview) {
+		listview.can_create = false;
+		listview.page.clear_primary_action();
+	},
 	refresh: function (listview) {
+		listview.can_create = false;
 		listview.page.clear_primary_action();
 	},
 };
