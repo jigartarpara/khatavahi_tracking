b18e9c5bc17ec09b139dac8b0de27b1d3c90bd9e
Hiren Savaliya <dev101@khatavahi.in>
2026-05-15 11:44:47 +0530

    feat: updated user permissions

--- khatavahi_tracking/khatavahi_tracking/doctype/tracking_setup_kbs/tracking_setup_kbs.py
+++ khatavahi_tracking/khatavahi_tracking/doctype/tracking_setup_kbs/tracking_setup_kbs.py
@@ -26,6 +26,9 @@ SALES_USER_PERMISSIONS = {
     "Warehouse": {"select": 1, "read": 1, "export": 1},
     "Global Defaults": {"select": 1, "read": 1, "export": 1},
     "Currency": {"select": 1, "read": 1, "export": 1},
+    "Sales Order": {"select": 1, "read": 1, "write": 1, "create": 1, "delete": 1, "export": 1, "if_owner": 1},
+    "Sales Person": {"select": 1, "read": 1, "export": 1},
+    "Employee": {"select": 1, "read": 1, "export": 1},
 }
 
 SUPPORT_USER_PERMISSIONS = {
