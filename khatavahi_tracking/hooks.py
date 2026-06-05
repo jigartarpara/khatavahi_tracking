3dcec846565fa6629f61bb2472a466da426c2a6d
Hiren Savaliya <dev101@khatavahi.in>
2026-06-05 15:30:08 +0530

    Revert "refactor: remove Book Order doctype and associated components from tracking module"
    This reverts commit 2036bcc1896ddc9815c08e2bfa7369401a36d826.
    
    # Conflicts:
    #	khatavahi_tracking/khatavahi_tracking/doctype/tracking_setup_kbs/tracking_setup_kbs.py

--- khatavahi_tracking/hooks.py
+++ khatavahi_tracking/hooks.py
@@ -139,7 +139,14 @@ after_migrate = "khatavahi_tracking.khatavahi_tracking.setup.after_migrate"
 # ---------------
 # Hook on document methods and events
 
-doc_events = {}
+doc_events = {
+	"Sales Order": {
+		"on_update": "khatavahi_tracking.khatavahi_tracking.utils.sync_book_order_status",
+		"on_submit": "khatavahi_tracking.khatavahi_tracking.utils.sync_book_order_status",
+		"on_cancel": "khatavahi_tracking.khatavahi_tracking.utils.sync_book_order_status",
+		"on_trash": "khatavahi_tracking.khatavahi_tracking.utils.sync_book_order_status",
+	}
+}
 
 # Scheduled Tasks
 # ---------------
--- khatavahi_tracking/khatavahi_tracking/utils.py
+++ khatavahi_tracking/khatavahi_tracking/utils.py
@@ -1,3 +1,49 @@
 import frappe
 
-# Removed Book Order related utility functions as per request.
+def sync_book_order_status(doc, method):
+	if not doc.get("book_order"):
+		return
+
+	if method == "on_cancel" or method == "on_trash" or doc.docstatus == 2:
+		frappe.db.set_value("Book Order", doc.book_order, "status", "Pending")
+		return
+
+	status_map = {
+		0: "Sales Order Draft",
+		1: "Sales Order Submitted",
+		2: "Sales Order Cancelled"
+	}
+
+	new_status = status_map.get(doc.docstatus)
+	
+	if new_status:
+		frappe.db.set_value("Book Order", doc.book_order, "status", new_status)
+
+@frappe.whitelist()
+def make_sales_order(source_name):
+	target_doc = frappe.new_doc("Sales Order")
+	source_doc = frappe.get_doc("Book Order", source_name)
+	
+	target_doc.customer = source_doc.customer
+	target_doc.delivery_date = source_doc.delivey_date
+	target_doc.book_order = source_doc.name
+	target_doc.selling_price_list = source_doc.default_price_list
+	target_doc.set_warehouse = source_doc.source_warehouse
+	
+	for item in source_doc.item:
+		target_doc.append("items", {
+			"item_code": item.item,
+			"qty": item.qty,
+			"uom": item.uom,
+			"delivery_date": source_doc.delivey_date,
+			"rate": item.price
+		})
+
+	if source_doc.sales_person_id:
+		target_doc.append("sales_team", {
+			"sales_person": source_doc.sales_person_id,
+			"allocated_percentage": 100
+		})
+	
+	target_doc.set_missing_values()
+	return target_doc
